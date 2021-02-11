0998db4a11293ac8b321d5bb510e0cca3bbeca14
miladsiddiquey <75581636+miladsiddiquey@users.noreply.github.com>
2021-02-11 21:48:14 +0600

    some code change

--- app.js
+++ app.js
@@ -7,21 +7,21 @@ const sr = ScrollReveal({
     reset: true
 });
 
-/*SCROLL ABOUT*/
-sr.reveal('.about__img', {});
-sr.reveal('.about__subtitle', { delay: 400 });
-sr.reveal('.about__text', { delay: 400 });
+// /*SCROLL ABOUT*/
+// sr.reveal('.about__img', {});
+// sr.reveal('.about__subtitle', { delay: 400 });
+// sr.reveal('.about__text', { delay: 400 });
 
-/*SCROLL SKILLS why you choose us*/
-sr.reveal('.subtitle', {});
-sr.reveal('.skills__img', { delay: 600 });
+// /*SCROLL SKILLS why you choose us*/
+// sr.reveal('.subtitle', {});
+// sr.reveal('.skills__img', { delay: 600 });
 
-/*OUR SERVICES*/
-sr.reveal('.service__img',{interval: 200});
+// /*OUR SERVICES*/
+// sr.reveal('.service__img',{interval: 200});
 
-/*SCROLL WORK*/
-sr.reveal('.work__img',{interval: 200});
-/*SCROLL CONTACT*/
+// /*SCROLL WORK*/
+// sr.reveal('.work__img',{interval: 200});
+// /*SCROLL CONTACT*/
 
-sr.reveal('.contact__text', { delay: 300 });
+// sr.reveal('.contact__text', { delay: 300 });
 
